28ca45e1f0eda145777f7f5dd4c2bed202273138
Martin Przystałka <Jenkins.Heisenberg@performgroup.com>
2019-05-10 17:02:58 +0200

    initial commit.

---
+++ Calculator/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by martin.przystalka on 10/05/2019.
+//  Copyright © 2019 Martin Przystalka. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ Calculator/CalculatorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by martin.przystalka on 10/05/2019.
+//  Copyright © 2019 Martin Przystalka. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+}
